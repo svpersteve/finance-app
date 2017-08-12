a41d9a976a4af8f8bb88d4127bcec269995785bb
Steve Brewer <steve.brewer@futurelearn.com>
2017-08-12 15:43:27 +0100

    Add position to order accounts

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,9 +1,9 @@
 class Account < ActiveRecord::Base
   has_many :balances
 
-  scope :short_term, -> { where(category: 'short-term') }
-  scope :long_term, -> { where(category: 'long-term') }
-  scope :property, -> { where(category: 'property') }
+  scope :short_term, -> { where(category: 'short-term').order('position ASC') }
+  scope :long_term, -> { where(category: 'long-term').order('position ASC') }
+  scope :property, -> { where(category: 'property').order('position ASC') }
 
   def current_balance
     balances&.last&.amount || 0
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170812140650) do
+ActiveRecord::Schema.define(version: 20170812144025) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 20170812140650) do
     t.datetime "updated_at", null: false
     t.string "category"
     t.float "interest_rate"
+    t.integer "position"
   end
 
   create_table "balances", force: :cascade do |t|
