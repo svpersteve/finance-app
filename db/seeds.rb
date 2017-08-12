cd0475203e86846af3b1fdbf4c68fdcdc1ec9372
Steve Brewer <steve.brewer@futurelearn.com>
2017-08-12 14:43:24 +0100

    Add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Account.create(name: 'Starling', category: 'short-term')
+Account.create(name: 'MBNA', category: 'short-term')
+Account.create(name: 'Mortgage', category: 'property')
+Account.create(name: 'Flat', category: 'property')
+Account.create(name: 'FirstBuy', category: 'property')
+Account.create(name: 'Aaron', category: 'short-term')
+Account.create(name: 'MoneyBox', category: 'long-term')
+Account.create(name: 'Loan One', category: 'long-term')
+Account.create(name: 'Loan Two', category: 'long-term')
