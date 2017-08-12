6d55273dad9095d62d93c8d48bbdbdbe4960030f
Steve Brewer <steve.brewer@futurelearn.com>
2017-08-12 19:19:06 +0100

    Add basic authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+    before_action :authenticate
+
+    def authenticate
+      if Rails.env == 'production'
+        authenticate_or_request_with_http_basic do |user, password|
+          user == "steve" && password == ENV['PASSWORD']
+        end
+      end
+    end
 end
