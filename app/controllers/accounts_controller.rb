4cacc8e627b3a51c2cb43ba24bc2050a9debd8cf
Steve Brewer <steve.brewer@futurelearn.com>
2017-08-12 21:51:31 +0100

    Add graph for account balance history

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -5,5 +5,21 @@ class AccountsController < ApplicationController
 
   def show
     @account = Account.find(params[:id])
+    get_chart_data
+  end
+
+  private
+
+  def get_chart_data
+    @dates = []
+    @balances = []
+
+    @account.balances.in_date_order.each do |balance|
+      @dates << balance.created_at.to_date.to_formatted_s(:long_ordinal)
+      @balances << balance.amount
+    end
+
+    gon.dates = @dates
+    gon.balances = @balances
   end
 end
